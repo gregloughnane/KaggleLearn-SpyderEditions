e4cc015f8ba2d04db70c8624ac77dd05c448c461
Greg Loughnane <48775140+gregloughnane@users.noreply.github.com>
2019-04-29 15:48:33 -0400

    Create Python_Intro.py

---
+++ 1-Intro-To-Machine-Learning/0-Python-Intro/Python_Intro.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 15:39:29 2019
+
+Make sure that these libraries are in your Anaconda Virtual Environment
+- None!  Just open the environment
+
+@author: Greg Loughnane
+"""
