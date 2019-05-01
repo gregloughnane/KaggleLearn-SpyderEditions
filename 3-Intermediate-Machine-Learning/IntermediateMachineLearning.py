911fdb4366ce403c693dc43baf1d74373cf143c1
Greg Loughnane <48775140+gregloughnane@users.noreply.github.com>
2019-05-01 14:13:41 -0400

    Create IntermediateMachineLearning.py

---
+++ 3-Intermediate-Machine-Learning/IntermediateMachineLearning.py
@@ -0,0 +1,461 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 13:13:56 2019 (Python 3.6)
+
+Make sure that these libraries are in your Anaconda Virtual Environment
+- pandas        (I ran with 0.24.2)
+- scikit-learn  (I ran with 0.20.3)
+- xgboost       (I ran with 0.82)
+
+and...
+
+Make sure that the following data sets are in your working directory in Spyder
+- melb_data.csv
+- AER_credit_card_data.csv
+
+also...
+
+You should open and run IntroToMachineLearning.py first before taking this on
+
+@author: Greg Loughnane
+"""
+
+#--------------------------------Missing Values--------------------------------
+print('--------------------------------Missing Values--------------------------------')
+
+import time
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load the data
+data = pd.read_csv('melb_data.csv')
+
+# Select target
+y = data.Price
+
+# To keep things simple, we'll use only numerical predictors
+melb_predictors = data.drop(['Price'], axis=1)
+X = melb_predictors.select_dtypes(exclude=['object'])
+
+# Divide data into training and validation subsets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=10)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+# Get names of columns with missing values
+cols_with_missing = [col for col in X_train.columns
+                     if X_train[col].isnull().any()]
+
+# Approach 1: Drop Missing Data
+start_time = time.time()
+
+# Drop columns in training and validation data
+reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
+
+print()
+print("MAE from Approach 1 (Drop columns with missing values):")
+print(score_dataset(reduced_X_train, reduced_X_valid, y_train, y_valid))
+
+
+from sklearn.impute import SimpleImputer
+
+# Imputation
+my_imputer = SimpleImputer()
+imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print('-----------------------------This approach took : ',timing, 's to run')
+    
+# Approach 2: Imputation
+start_time = time.time()
+
+# Imputation removed column names; put them back
+imputed_X_train.columns = X_train.columns
+imputed_X_valid.columns = X_valid.columns
+
+print()
+print("MAE from Approach 2 (Imputation):")
+print(score_dataset(imputed_X_train, imputed_X_valid, y_train, y_valid))
+
+# Shape of training data (num_rows, num_columns)
+print(X_train.shape)
+
+# Make copy to avoid changing original data (when imputing)
+X_train_plus = X_train.copy()
+X_valid_plus = X_valid.copy()
+
+# Make new columns indicating what will be imputed
+for col in cols_with_missing:
+    X_train_plus[col + '_was_missing'] = X_train_plus[col].isnull()
+    X_valid_plus[col + '_was_missing'] = X_valid_plus[col].isnull()
+
+# Imputation
+my_imputer = SimpleImputer()
+imputed_X_train_plus = pd.DataFrame(my_imputer.fit_transform(X_train_plus))
+imputed_X_valid_plus = pd.DataFrame(my_imputer.transform(X_valid_plus))
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print('-----------------------------This approach took : ',timing, 's to run')
+
+# Approach 3: Extension to Imputation
+start_time = time.time()
+
+# Imputation removed column names; put them back
+imputed_X_train_plus.columns = X_train_plus.columns
+imputed_X_valid_plus.columns = X_valid_plus.columns
+
+print()
+print("MAE from Approach 3 (An Extension to Imputation):")
+print(score_dataset(imputed_X_train_plus, imputed_X_valid_plus, y_train, y_valid))
+
+# Number of missing values in each column of training data
+missing_val_count_by_column = (X_train.isnull().sum())
+print()
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print('-----------------------------This approach took : ',timing, 's to run')
+
+#-----------------------------Categorical Variables----------------------------
+print('-----------------------------Categorical Variables----------------------------')
+
+# Separate target from predictors
+y = data.Price
+X = data.drop(['Price'], axis=1)
+
+# Divide data into training and validation subsets
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+# Drop columns with missing values (simplest approach)
+cols_with_missing = [col for col in X_train_full.columns if X_train_full[col].isnull().any()] 
+X_train_full.drop(cols_with_missing, axis=1, inplace=True)
+X_valid_full.drop(cols_with_missing, axis=1, inplace=True)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and 
+                        X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = low_cardinality_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+
+# View the categorical data
+print()
+print(X_train.head())
+
+# Get list of categorical variables
+s = (X_train.dtypes == 'object')
+object_cols = list(s[s].index)
+
+print("Categorical variables:")
+print(object_cols)
+
+# Approach 1: Drop Categorical Variables
+start_time = time.time()
+print('--------------------------------------------------')
+drop_X_train = X_train.select_dtypes(exclude=['object'])
+drop_X_valid = X_valid.select_dtypes(exclude=['object'])
+
+print()
+print("MAE from Approach 1 (Drop categorical variables):")
+print(score_dataset(drop_X_train, drop_X_valid, y_train, y_valid))
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print('-----------------------------This approach took : ',timing, 's to run')
+
+# Approach 2: Label Encoding
+start_time = time.time()
+from sklearn.preprocessing import LabelEncoder
+
+# Make copy to avoid changing original data 
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+# Apply label encoder to each column with categorical data
+label_encoder = LabelEncoder()
+for col in object_cols:
+    label_X_train[col] = label_encoder.fit_transform(X_train[col])
+    label_X_valid[col] = label_encoder.transform(X_valid[col])
+
+print()
+print("MAE from Approach 2 (Label Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print('-----------------------------This approach took : ',timing, 's to run')
+
+# Approach 3: One-Hot Encoding
+start_time = time.time()
+from sklearn.preprocessing import OneHotEncoder
+
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+print()
+print("MAE from Approach 3 (One-Hot Encoding):") 
+print(score_dataset(OH_X_train, OH_X_valid, y_train, y_valid))
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print('-----------------------------This approach took : ',timing, 's to run')
+
+#----------------------------------Pipelines-----------------------------------
+print('----------------------------------Pipelines-----------------------------------')
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Separate target from predictors
+y = data.Price
+X = data.drop(['Price'], axis=1)
+
+# Divide data into training and validation subsets
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+categorical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and 
+                        X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = categorical_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+
+# View the categorical data
+print()
+print(X_train.head())
+
+# Step 1: Define pre-processing steps
+start_time = time.time()
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='constant')
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print()
+print('Step 1: Define pre-processing steps, took : ',timing, 's to run')
+
+# Step 2: Define the model
+start_time = time.time()
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print()
+print('Step 2: Define the model, took : ',timing, 's to run')
+
+# Step 3: Create and Evaluate Pipeline
+start_time = time.time()
+from sklearn.metrics import mean_absolute_error
+
+# Bundle preprocessing and modeling code in a pipeline
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
+                             ])
+
+# Preprocessing of training data, fit model 
+my_pipeline.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = my_pipeline.predict(X_valid)
+
+# Evaluate the model
+score = mean_absolute_error(y_valid, preds)
+
+print()
+print('MAE:', score)
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print('Step 3: Create and Evaluate Pipeline, took : ',timing, 's to run')
+
+#-------------------------------Cross Validation-------------------------------
+print('-------------------------------Cross Validation-------------------------------')
+
+
+
+# Read the data
+data = pd.read_csv('melb_data.csv')
+
+# Select subset of predictors
+cols_to_use = ['Rooms', 'Distance', 'Landsize', 'BuildingArea', 'YearBuilt']
+X = data[cols_to_use]
+
+# Select target
+y = data.Price
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+start_time = time.time()
+my_pipeline = Pipeline(steps=[('preprocessor', SimpleImputer()),
+                              ('model', RandomForestRegressor(n_estimators=50,
+                                                              random_state=0))
+                             ])
+                              
+from sklearn.model_selection import cross_val_score
+
+# Multiply by -1 since sklearn calculates *negative* MAE
+scores = -1 * cross_val_score(my_pipeline, X, y,
+                              cv=5,
+                              scoring='neg_mean_absolute_error')
+print()
+print("MAE scores:\n", scores)
+print()
+print("Average MAE score (across experiments):")
+print(scores.mean())
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print()
+print('The RandomForestRegressor with cross validation took : ',timing, 's to run')
+
+#-----------------------------------XG Boost-----------------------------------
+print('-----------------------------------XG Boost-----------------------------------')
+
+from xgboost import XGBRegressor
+
+start_time = time.time()
+
+# Separate data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+
+# Build and run model
+my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05, n_jobs=4)
+my_model.fit(X_train, y_train, 
+             early_stopping_rounds=5, 
+             eval_set=[(X_valid, y_valid)], 
+             verbose=False)
+
+from sklearn.metrics import mean_absolute_error
+
+predictions = my_model.predict(X_valid)
+print()
+print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_valid)))
+
+# Cacluate and show timing
+end_time = time.time()
+timing = end_time - start_time
+print()
+print('The XGBoostRegressor model took : ',timing, 's to run')
+
+#---------------------------------Data Leakage---------------------------------
+print('---------------------------------Data Leakage---------------------------------')
+
+# Read the data
+data = pd.read_csv('AER_credit_card_data.csv', 
+                   true_values = ['yes'], false_values = ['no'])
+
+# Select target
+y = data.card
+
+# Select predictors
+X = data.drop(['card'], axis=1)
+
+print("Number of rows in the dataset:", X.shape[0])
+print(X.head())
+
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+# Since there is no preprocessing, we don't need a pipeline (used anyway as best practice!)
+my_pipeline = make_pipeline(RandomForestClassifier(n_estimators=100))
+cv_scores = cross_val_score(my_pipeline, X, y, 
+                            cv=5,
+                            scoring='accuracy')
+
+print()
+print("Cross-validation accuracy: %f" % cv_scores.mean())
+
+expenditures_cardholders = X.expenditure[y]
+expenditures_noncardholders = X.expenditure[~y]
+
+print()
+print('Fraction of those who did not receive a card and had no expenditures: %.2f' \
+      %((expenditures_noncardholders == 0).mean()))
+print()
+print('Fraction of those who received a card and had no expenditures: %.2f' \
+      %(( expenditures_cardholders == 0).mean()))
+
+# Drop leaky predictors from dataset
+potential_leaks = ['expenditure', 'share', 'active', 'majorcards']
+X2 = X.drop(potential_leaks, axis=1)
+
+# Evaluate the model with leaky predictors removed
+cv_scores = cross_val_score(my_pipeline, X2, y, 
+                            cv=5,
+                            scoring='accuracy')
+
+print()
+print("Cross-val accuracy: %f" % cv_scores.mean())
